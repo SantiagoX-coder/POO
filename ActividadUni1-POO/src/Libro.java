67ad5e31923c04e1e5d1dd0fcc92ec4703883147
SantiagoX-Coder <sgamarrag@unicartagena.edu.co>
2025-09-14 14:37:12 -0500

    Agrego configuraciones de IntelliJ

---
+++ ActividadUni1-POO/src/Libro.java
@@ -0,0 +1,10 @@
+// Ejercicio 1: Crear una clase Libro
+//Crea una clase Libro con las siguientes propiedades: titulo, autor, numeroPaginas. Define:
+//1. Un constructor por defecto que asigne valores predeterminados.
+//2. Un constructor parametrizado que permita asignar valores específicos.
+//3. Métodos para mostrar los detalles del libro.
+public class Libro {
+    private String titulo;
+    private String autor;
+    private int NúmerodePaginas;
+}
