72fdb40b4c365fa8a5e71b47f393f47971b0a1b9
SantiagoX-Coder <sgamarrag@unicartagena.edu.co>
2025-09-14 20:27:45 -0500

    initial commit

--- ActividadUni1-POO/Taller1/Libro.java
+++ ActividadUni1-POO/Taller1/Libro.java
@@ -5,21 +5,23 @@
 // 2. Un constructor parametrizado que permita asignar valores específicos.
 // 3. Métodos para mostrar los detalles del libro.
 
+//Aplicamos la clase (Libro) para moldear y definir las propiedades y los comportamiento que tenga esta.
 public class Libro {
-    // 👾 Propiedades
+    // 👾 Propiedades (atributos) caracteristiocas del objeto a presentar en este caso: Autor, titulo y número de paginas
     String autor;
     String titulo;
     int numeroPaginas;
 
+    //Implementamos los construres (por defecto, sobrecargado y parametrizado) para inicializar el/los objetos al momento de crealos
+
     // Constructor solo con autor, (ejemplo de sobrecarga)
     public Libro(String autor) {
-       // Usamos metodo this. para
+        // Usamos metodo this. para
         this.autor = autor;
         this.titulo = "Sin titulo";
         this.numeroPaginas = 0;
 
     }
-
 
     // Constructor por defecto
     public Libro() {
@@ -30,29 +32,26 @@ public class Libro {
 
     // Constructor parametrizado
     public Libro(String titulo, String autor, int numeroPaginas) {
+        this(autor);
         this.autor = autor;
         this.titulo = titulo;
         this.numeroPaginas = numeroPaginas;
     }
 
-    // Metodo para mostrar detalles
+    // Adoptamos los metodos toStirng y mostrarDetalles para que los objetos realicen las acciones deseadas
     public void mostrarDetalles() {
         System.out.println("Título: " + titulo);
         System.out.println("Autor: " + autor);
         System.out.println("Número de páginas: " + numeroPaginas);
     }
 
-    // Metodo toString para que la representacion textual sea legible al correr el programa
+    // Metodo toString para que la representacion textual sea legible al correr el programa y el @override para redifinir este metodo heredado...
+    //...y se pueda adaptar a la clase.
     @Override
-    public String toString () {
+    public String toString() {
         return "Libro{titulo='" + titulo + "', autor='" + autor + "', numeroPaginas=" + numeroPaginas + "}";
     }
-    //Utilizamos clase Main para poder correr el programa
-public class Main{
-        public static void main(String[] args) {
-            Libro libro1 = new Libro();
-            libro1.mostrarDetalles();
-        }
-}
 
-}
+ }
+
+
